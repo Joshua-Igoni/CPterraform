1ca13eae87d43967e15024449fb58016b6c63f5e
Igoni <joshua.igoni@enocean.com>
2025-08-03 22:51:10 +0200

    updating code

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -34,11 +34,6 @@ locals {
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.this.id
   tags   = { Name = "${var.name}-igw" }
-  depends_on = [
-    aws_nat_gateway.nat[0],
-    aws_nat_gateway.nat[1],
-    aws_nat_gateway.nat[2],
-  ]
 }
 
 resource "aws_route_table" "public" {
